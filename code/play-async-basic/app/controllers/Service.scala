fe5a09db93b23b4bac0bb0c4a8f9d83bf836db09
Oscar Renalias <oscar@renalias.net>
2012-11-06 00:20:34 +0200

    No time to finish it.

--- code/play-async-basic/app/controllers/Service.scala
+++ code/play-async-basic/app/controllers/Service.scala
@@ -6,7 +6,7 @@ import play.api.libs.json._
 import play.api.Logger
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
-import play.api.libs.concurrent.Promise
+import play.api.libs.concurrent.{Akka, Promise}
 
 object Service extends Controller {
 
@@ -21,34 +21,6 @@ object Service extends Controller {
         }
 
         Ok(Json.toJson(repoList))
-      }
-    }
-  }
-
-  /**
-   * Execute multiple calls in parallel
-   */
-  def repoBranches(user: String) = Action {
-    Async {
-      WS.url("https://api.github.com/users/" + user + "/repos").get().map { response =>
-        val responses = (response.json \\ "name").map { repo =>
-          // return the Future
-          WS.url("https://api.github.com/repos/" + user + "/" + repo.as[String] + "/branches").get()
-        }
-
-        /*val results = requests.map { req =>
-          req.map { resp =>
-            JsObject(List("repo" -> JsString("TODO"), "branches" -> JsArray((resp.json \\ "name"))))
-          }
-        }*/
-
-        //def conv(p:Promise[play.api.libs.ws.Response]) = p.map { resp => resp.json \\ "name" }
-        Logger.debug(responses.map(promise => {
-          promise.map(response => response.json \\ "name")
-        }).mkString("-"))
-
-        //Ok(Json.toJson(results))
-        Ok(Json.toJson(JsObject(List("ok" -> JsString("ok")))))
       }
     }
   }
