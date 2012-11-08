1557657149e457156c85682f1c95f71eef6dea76
Oscar Renalias <oscar@renalias.net>
2012-11-08 23:35:25 +0200

    Added a little bit of extra documentation

--- code/play-async-basic/app/controllers/PromiseExample.scala
+++ code/play-async-basic/app/controllers/PromiseExample.scala
@@ -9,20 +9,30 @@ import play.api.mvc.{Controller, Action, AsyncResult}
  */
 object PromiseExample extends Controller {
 
+  //
   // Global promise that all actions will wait for
+  //
   val promise = Promise[String]()
 
-  // action that waits until the promise has been redeemed before sending a response to clients
+  //
+  // Action that waits until the promise has been redeemed before sending a response to clients
+  //
   def waitForPromise = Action {
     new AsyncResult(promise.map(s => Ok(s)))
   }
 
-  // renders our iframes
+  //
+  // Renders our iframes
+  //
   def index = Action {
     Ok(views.html.frames())
   }
 
-  // redeems the promise by providing a value
+  //
+  // Redeems the promise by providing a value; when the promise is redeemed, all clients waiting
+  // for it (in this case 100 iframes) will be unblocked and will display whatever text is received
+  // as a parameter.
+  //
   def redeemPromise(s:String) = Action {
     promise.redeem(s)
     Ok("Done!")
